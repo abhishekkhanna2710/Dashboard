be18ef3830ddb1ce36aa5bc989b92d109e2114b8
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-03-08 22:03:51 +0530

    updating_logo

--- client/src/components/layout/title/index.tsx
+++ client/src/components/layout/title/index.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useRouterContext, TitleProps } from "@pankod/refine-core";
 import { Button } from "@pankod/refine-mui";
 
-import {logo, yariga} from "assets"
+// import { logo, yariga } from "assets"
+import logos from "assets/main_logo.jpg"
+// import logos from "assets/logo_one.jpeg"
+import mainLogo from "assets/logo_one.jpg"
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
   const { Link } = useRouterContext();
@@ -11,9 +14,9 @@ export const Title: React.FC<TitleProps> = ({ collapsed }) => {
     <Button fullWidth variant="text" disableRipple>
       <Link to="/">
         {collapsed ? (
-          <img src={logo} alt="Refine" width="28px" />
+          <img src={mainLogo} alt="Refine" width="45px" />
         ) : (
-          <img src={yariga} alt="Refine" width="140px" />
+          <img src={logos} alt="Refine" width="170px" />
         )}
       </Link>
     </Button>
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { useLogin } from "@pankod/refine-core";
 import { Container, Box } from "@pankod/refine-mui";
 
-import { logo } from "../assets"
+import logos from "../assets/main_logo.jpg"
 
 import { CredentialResponse } from "../interfaces/google";
 
@@ -66,7 +66,7 @@ export const Login: React.FC = () => {
           }}
         >
           <div>
-            <img src={logo} alt="Refine Logo" />
+            <img src={logos} alt="Refine Logo" />
           </div>
           <Box mt={4}>
             <GoogleButton />
